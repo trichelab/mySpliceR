057ed4efcffccd4e22d3eb0fc24e56326cf023e4
huangyizR <peterhuang319@gmail.com>
2025-04-03 13:15:38 -0400

    fix reference if python script in parse_gtf_to_bed.R.

--- R/parse_gtf_to_bed.R
+++ R/parse_gtf_to_bed.R
@@ -9,7 +9,11 @@
 #' parse_gtf_to_bed("gencode.v36.annotation.gtf", "junctions.bed")
 
 parse_gtf_to_bed <- function(gtf_path, output_bed_path = NULL) {
-  reticulate::source_python("parse_gtf_to_junction_bed.py")
+  py_file <- system.file("python/parse_gtf_to_junction_bed.py", package = "mySpliceR")
+  if (py_file == "") stop("Python script not found in package.")
+
+  reticulate::source_python(py_file)
+
   if (is.null(output_bed_path)) {
     return(parse_gtf_to_junction_bed(gtf_path))
   } else {
